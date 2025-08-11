0c31964fefb42fdb2a5a6c290b8eda52174bb516
Nico44S <nico.44378189@gmail.com>
2025-08-10 22:05:12 -0300

    Ejercicio 1 -  hola mundo.

---
+++ Practica_Fase1/Entrega_1/ejercicio1.c
@@ -0,0 +1,16 @@
+/*
+Desarrollar un programa que muestre por `STDOUT` el mensaje "Hola Mundo C!".
+
+Indicar en un comentario la instrucción de compilación con GCC por línea de comandos.
+-----------------
+Nombre y Apelido: Nicolas Savone
+Usuario Github: Nico44S
+*/
+
+#include <stdio.h>
+
+int main ()
+{
+    printf ("Hola Mundo.");
+    return 0;
+}
